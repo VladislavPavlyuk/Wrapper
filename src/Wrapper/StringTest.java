46e71f22d5f9eca7af83d372d4b712bef4148923
Vladislav Pavlyuk <vladpavliuk@gmail.com>
2024-08-31 15:04:44 +0200

    StringTest Class, WrapperTest Class

--- src/Wrapper/StringTest.java
+++ src/Wrapper/StringTest.java
@@ -1,2 +1,117 @@
-package Wrapper;public class StringTest {
+package Wrapper;
+
+import java.util.Scanner;
+
+public class StringTest {
+    public static void main(String[] args) {
+        // 1) Литерал
+        String literalString = "Пример строки";
+
+        // 2) new
+        String newStr = new String("Пример строки");
+
+        // 3) Массив символов
+        char[] charArray = {'П', 'р', 'и', 'м', 'е', 'р', ' ', 'с', 'т', 'р', 'о', 'к', 'и'};
+        String charArrayString = new String(charArray);
+
+        // 4) Из байтов
+        byte[] byteArray = {80, 114, 105, 109, 101, 114, 32, 115, 116, 114, 111, 107, 105};
+        String byteArrayString = new String(byteArray);
+
+        // 5) С помощью StringBuilder
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Пример строки");
+        String stringBuilderString = stringBuilder.toString();
+
+        // Вывод всех строк
+        System.out.println("Литерал: " + literalString);
+        String newString = null;
+        System.out.println("new: " + newString);
+        System.out.println("Массив символов: " + charArrayString);
+        System.out.println("Из байтов: " + byteArrayString);
+        System.out.println("StringBuilder: " + stringBuilderString);
+
+        String fruits = "Апельсин,Яблоко,Гранат,Груша";
+
+        String[] fruitArray = fruits.split(",");
+
+        String longestFruit = "";
+        for (String fruit : fruitArray) {
+            if (fruit.length() > longestFruit.length()) {
+                longestFruit = fruit;
+            }
+        }
+        System.out.println("Самое длинное название фрукта: " + longestFruit.toLowerCase());
+
+        for (String fruit : fruitArray) {
+            String shortName = fruit.substring(0, 3);
+            System.out.println("Сокращенное название: " + shortName);
+        }
+
+        newString = "   Я_новая_строка      ";
+
+        newString = newString.trim();
+
+        newString = newString.replace('_', ' ');
+
+        System.out.println("Окончательный вариант строки: " + newString);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите текст: ");
+        String inputText = scanner.nextLine();
+        System.out.println("Вы ввели: " + inputText);
+
+        if (inputText.startsWith("Запуск")) {
+            System.out.println("Запускаем процесс");
+        }
+
+        if (inputText.endsWith("завершен")) {
+            System.out.println("Процесс завершен");
+        }
+
+        if (inputText.toLowerCase().contains("ошибка")) {
+            System.out.println("Произошла ошибка");
+        }
+
+        // Создаем строки
+        String str1 = "Первая строка";
+        String str2 = "Вторая строка";
+        String str3 = "Третья строка";
+        String str4 = "Четвертая строка";
+        String str5 = "Пятая строка";
+        String str6 = "Шестая строка";
+
+        // Создаем StringBuilder
+        StringBuilder sb = new StringBuilder();
+
+        // Добавляем строки в StringBuilder
+        sb.append(str1).append("\n");
+        sb.append(str2).append("\n");
+        sb.append(str3).append("\n");
+        sb.append(str4).append("\n");
+        sb.append(str5).append("\n");
+        sb.append(str6).append("\n");
+
+        // Разбиваем строки на массив
+        String[] lines = sb.toString().split("\n");
+
+        // Создаем новый StringBuilder для результата
+        StringBuilder result = new StringBuilder();
+
+        // Добавляем строки с переносом после каждой третьей строки
+        for (int i = 0; i < lines.length; i++) {
+            result.append(lines[i]);
+            if ((i + 1) % 3 == 0) {
+                result.append("\n");
+            }
+        }
+
+        // Разворачиваем содержимое задом наперед
+        result.reverse();
+
+        // Преобразуем StringBuilder в строку и выводим на экран
+        System.out.println(result.toString());
+
+
+    }
 }
--- src/Wrapper/WrapperTest.java
+++ src/Wrapper/WrapperTest.java
@@ -1,2 +1,111 @@
-package Wrapper;public class WrapperTest {
+package Wrapper;
+
+public class WrapperTest {
+
+    public static void main(String[] args) {
+
+        //2
+        //String - stores text, such as "Hello". String values are surrounded by double quotes
+        String s1 = "Hello";
+        String s2 = String.valueOf("Hello");
+        String s3 = new String("Hello");
+        String s4 = Integer.toString(10);
+
+        //Integer - stores integers (whole numbers), without decimals, such as 123 or -123
+        Integer i1 = 10;
+        Integer i2 = Integer.valueOf(10);
+        //Integer i3 = new Integer(10); @Depricated since version 9
+        Integer i4 = Integer.parseInt("10");
+
+        //Float - stores floating point numbers, with decimals, such as 19.99 or -19.99
+        Float f1 = 19.99F;
+        Float f2 = Float.valueOf(19.99F);
+        //Float f3 = new Float(19.99F); @Depricated since version 9
+        Float f4 = Float.parseFloat("19.99");
+
+        //Double
+        Double d1 = 19.99D;
+        Double d2 = Double.valueOf(19.99);
+        //Double d3 = new Double(19.99D); @Depricated since version 9
+        Double d4 = Double.parseDouble("19.99");
+
+        //char - stores single characters, such as 'a' or 'B'. Char values are surrounded by single quotes
+        char c1 = 'A';
+        char c2 = Character.valueOf('A');
+        //char c3 = new Character('A'); @Depricated since version 9
+        char[] c4 = s1.toCharArray();
+
+        //boolean - stores values with two states: true or false
+        boolean b1 = true;
+        boolean b2 = Boolean.valueOf(false);
+        //boolean b3 = new Boolean(true); @Depricated since version 9
+        boolean b4 = Boolean.parseBoolean("false");
+
+        //3
+
+        Double d3 = Double.valueOf("3.14159265358979323846");
+
+        //byte, short, int, float, long
+        byte bMin = -128;
+        byte bMax = 127;
+
+        short sMin = -32768;
+        short sMax = 32767;
+
+        int iMin = -2147483648;
+        int iMax = 2147483647;
+
+        float fMin = -3.141592653589793F;
+        float fMax = 3.141592653589793F;
+
+        long lMin = -9223372036854775808L;
+        long lMax = 9223372036854775807L;
+
+        bMin = d3.byteValue();
+        sMin = d3.shortValue();
+        iMin = d3.intValue();
+        fMin = d3.floatValue();
+        lMin = d3.longValue();
+
+        // 4
+        // 4.1 Создать 2 переменные типа Double, одной из них присвоить значение 0.0
+        Double nonZeroValue = 1.0;
+        Double zeroValue = 0.0;
+
+        // 4.2 Создать переменную типа Double nanValue и присвоить ей значение, деления ненулевой переменной на нулевую
+        Double nanValue = nonZeroValue / zeroValue;
+
+        // 4.3 Создать переменную infinityValue и присвоить ей значение, деления нулевой переменной саму на себя
+        Double infinityValue = zeroValue / zeroValue;
+
+        // 4.4 Вывести nanValue и infinityValue на экран
+        System.out.println("nanValue: " + nanValue);
+        System.out.println("infinityValue: " + infinityValue);
+
+        // 4.5 Добавить по 2 блока if для каждого числа в которых проверяется результат выполнения методов isNaN и isInfinity
+        if (nanValue.isNaN()) {
+            System.out.println("Переменная nanValue = NaN");
+        }
+        if (infinityValue.isNaN()) {
+            System.out.println("Переменная infinityValue = NaN");
+        }
+        if (nanValue.isInfinite()) {
+            System.out.println("Переменная nanValue = Infinity");
+        }
+        if (infinityValue.isInfinite()) {
+            System.out.println("Переменная infinityValue = Infinity");
+        }
+
+        // 5. Создать 2 переменные типа Long и присвоить обеим значения 120
+        Long var1 = 120L;
+        Long var2 = 120L;
+
+        // 5.1 Вывести на экран результат сравнения этих переменных по оператору ==
+        System.out.println("Сравнение var1 и var2 (120L) по оператору ==: " + (var1 == var2));
+
+        // 5.2 Заменить значения переменных на 1200, перепроверить сравнение
+        var1 = 1200L;
+        var2 = 1200L;
+        System.out.println("Сравнение var1 и var2 (1200L) по оператору ==: " + (var1 == var2));
+    }
 }
